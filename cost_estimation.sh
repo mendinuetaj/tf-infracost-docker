4238e964d786e0889ee2be11df31f95c1314bbc8
jose <mendinuetaj2@gmail.com>
2023-02-27 21:57:51 -0500

    Add inputs and outputs

--- cost_estimation.sh
+++ cost_estimation.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo "This is what I got: $INFRACOST_API_KEY"
-cost=$(INFRACOST_API_KEY=$1 infracost breakdown --path /code/ --format json)
+cost=$(infracost breakdown --path /code/ --format json)
 cost=$(echo "$cost" | jq -Rsa .)
-echo "cost=$cost"
+echo "cost=$cost" >>"$GITHUB_OUTPUT"
