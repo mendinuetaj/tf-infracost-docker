8bdda1f0143884645bf5ffdcebfaff023fb41e79
jose <mendinuetaj2@gmail.com>
2023-02-28 17:08:10 -0500

    Add inputs and outputs

--- cost_estimation.sh
+++ cost_estimation.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-echo "terraform dir: $TERRAFORM_ROOT_DIR"
-cost=$(infracost breakdown --path "$TERRAFORM_ROOT_DIR" --format html)
-echo "cost=\"$cost\"" >>"$GITHUB_OUTPUT"
+echo "terraform code dir: $TERRAFORM_ROOT_DIR"
+cd "$TERRAFORM_ROOT_DIR" || exit 1
+infracost breakdown --path . --format json --out-file /tmp/infracost.json
+infracost comment github --path=/tmp/infracost.json \
+  --repo="$GITHUB_REPOSITORY" \
+  --pull-request="$PR_NUMBER" \
+  --github-token="$PERSONAL_TOKEN" \
+  --behavior=update
+exit $?
