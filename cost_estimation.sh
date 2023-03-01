5c30e55638d3361dfa486017c3cd5711f77e60e9
jose <mendinuetaj2@gmail.com>
2023-02-28 20:54:33 -0500

    Add inputs and outputs

--- cost_estimation.sh
+++ cost_estimation.sh
@@ -4,6 +4,7 @@ cd "$TERRAFORM_ROOT_DIR" || exit 1
 infracost breakdown --path . --format json --out-file /tmp/infracost.json
 infracost comment github --path=/tmp/infracost.json \
   --repo="$GITHUB_REPOSITORY" \
+  --github-api-url="$GITHUB_API_URL" \
   --pull-request="$PR_NUMBER" \
   --github-token="$PERSONAL_TOKEN" \
   --behavior=update
